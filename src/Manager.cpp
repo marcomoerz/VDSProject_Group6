a1ae3000c7f49cce3af591507e1cf3decead3cbc
Marco Mörz <64926170+marcomoerz@users.noreply.github.com>
2024-11-08 13:14:27 +0100

    Empty manager skeleton (#1)

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,1 +1,103 @@
 #include "Manager.h"
+
+#include <fstream>
+
+namespace ClassProject {
+
+Manager::Manager() {
+    // TODO
+}
+
+BDD_ID Manager::createVar(const std::string &label) {
+    return 0;
+}
+
+const BDD_ID &Manager::True() {
+    return 0;
+}
+
+const BDD_ID &Manager::False() {
+    return 0;
+}
+
+bool Manager::isConstant(BDD_ID f) {
+    return false;
+}
+
+bool Manager::isVariable(BDD_ID x) {
+    return false;
+}
+
+BDD_ID Manager::topVar(BDD_ID f) {
+    return 0;
+}
+
+BDD_ID Manager::ite(BDD_ID i, BDD_ID t, BDD_ID e) {
+    return 0;
+}
+
+BDD_ID Manager::coFactorTrue(BDD_ID f, BDD_ID x) {
+    return 0;
+}
+
+BDD_ID Manager::coFactorFalse(BDD_ID f, BDD_ID x) {
+    return 0;
+}
+
+BDD_ID Manager::coFactorTrue(BDD_ID f) {
+    return 0;
+}
+
+BDD_ID Manager::coFactorFalse(BDD_ID f) {
+    return 0;
+}
+
+BDD_ID Manager::and2(BDD_ID a, BDD_ID b) {
+    return 0;
+}
+
+BDD_ID Manager::or2(BDD_ID a, BDD_ID b) {
+    return 0;
+}
+
+BDD_ID Manager::xor2(BDD_ID a, BDD_ID b) {
+    return 0;
+}
+
+BDD_ID Manager::neg(BDD_ID a) {
+    return 0;
+}
+
+BDD_ID Manager::nand2(BDD_ID a, BDD_ID b) {
+    return 0;
+}
+
+BDD_ID Manager::nor2(BDD_ID a, BDD_ID b) {
+    return 0;
+}
+
+BDD_ID Manager::xnor2(BDD_ID a, BDD_ID b) {
+    return 0;
+}
+
+std::string Manager::getTopVarName(const BDD_ID &root) {
+    return "";
+}
+
+void Manager::findNodes(const BDD_ID &root, std::set<BDD_ID> &nodes_of_root) {
+    // TODO
+}
+
+void Manager::findVars(const BDD_ID &root, std::set<BDD_ID> &vars_of_root) {
+    // TODO
+}
+
+size_t Manager::uniqueTableSize() {
+    return 0;
+}
+
+void Manager::visualizeBDD(std::string filepath, BDD_ID &root) {
+    // TODO
+}
+
+} // namespace ClassProject
--- src/Manager.h
+++ src/Manager.h
@@ -7,8 +7,50 @@
 
 #include "ManagerInterface.h"
 
+#include <string>
+
 namespace ClassProject {
 
-}
+
+class Manager : public ManagerInterface {
+public:
+// Constructor
+    Manager(); // implemented in Manager.cpp
+    Manager(const Manager &mgr) = default; // copy constructor
+    Manager(Manager &&mgr) = default; // move constructor
+    Manager &operator=(const Manager &mgr) = default; // copy assignment
+    Manager &operator=(Manager &&mgr) = default; // move assignment
+// Destructor
+    ~Manager() /* override */ = default; // destructor // TODO should be virtual in ManagerInterface.h
+// ManagerInterface
+    BDD_ID createVar(const std::string &label) override;
+    const BDD_ID &True() override;
+    const BDD_ID &False() override;
+    bool isConstant(BDD_ID f) override;
+    bool isVariable(BDD_ID x) override;
+    BDD_ID topVar(BDD_ID f) override;
+    BDD_ID ite(BDD_ID i, BDD_ID t, BDD_ID e) override;
+    BDD_ID coFactorTrue(BDD_ID f, BDD_ID x) override;
+    BDD_ID coFactorFalse(BDD_ID f, BDD_ID x) override;
+    BDD_ID coFactorTrue(BDD_ID f) override;
+    BDD_ID coFactorFalse(BDD_ID f) override;
+    BDD_ID and2(BDD_ID a, BDD_ID b) override;
+    BDD_ID or2(BDD_ID a, BDD_ID b) override;
+    BDD_ID xor2(BDD_ID a, BDD_ID b) override;
+    BDD_ID neg(BDD_ID a) override;
+    BDD_ID nand2(BDD_ID a, BDD_ID b) override;
+    BDD_ID nor2(BDD_ID a, BDD_ID b) override;
+    BDD_ID xnor2(BDD_ID a, BDD_ID b) override;
+    std::string getTopVarName(const BDD_ID &root) override;
+    void findNodes(const BDD_ID &root, std::set<BDD_ID> &nodes_of_root) override;
+    void findVars(const BDD_ID &root, std::set<BDD_ID> &vars_of_root) override;
+    size_t uniqueTableSize() override;
+    void visualizeBDD(std::string filepath, BDD_ID &root) override;
+protected:
+// TODO Protected methods and variables
+
+};
+
+} // namespace ClassProject
 
 #endif
